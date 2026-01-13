81a58f4dc1ebeacb3cc0c418d38953736d9ba9b0
MaxLuc26 <maxlucasberman@gmail.com>
2026-01-13 15:56:56 +0200

    Update image_utils.py

--- image_utils.py
+++ image_utils.py
@@ -1,9 +1,29 @@
 from PIL import Image
 import numpy as np
 from scipy.signal import convolve2d
+import matplotlib as plt
 
-def load_image(path):
-    pass # Replace the `pass` with your code
+def load_image(file_path):
+  input_image = Image.open(file_path)
+  img_array = np.array(input_image)
+  return img_array
 
-def edge_detection(image):
-    pass # Replace the `pass` with your code
+from scipy.signal import convolve2d
+def edge_detection(loaded_img):
+  gray_tumor = np.mean(loaded_img, axis=2)
+  plt.imshow(gray_tumor, cmap='gray')
+
+  kernelY = np.array([[1, 2, 1],
+                      [0, 0, 0],
+                      [-1, -2, -1]])
+
+  kernelX = np.array([[-1, 0, 1],
+                      [-2, 0, 2],
+                      [-1, 0, 1]])
+
+  edge_x = convolve2d(gray_tumor, kernelX, mode='same', boundary='fill', fillvalue=0)
+  edge_y = convolve2d(gray_tumor, kernelY, mode='same', boundary='fill', fillvalue=0)
+
+  edge_mag = np.sqrt(edge_x**2 + edge_y**2)
+
+  return edge_mag
